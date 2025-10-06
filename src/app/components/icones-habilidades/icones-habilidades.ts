12efa4563d28ce34c751d2bfe5a12409fea01f1f
iagodizlla <iagoschlemper@gmail.com>
2025-10-06 20:49:11 -0300

    Implementado RxJS nas Tecnologias

--- src/app/components/icones-habilidades/icones-habilidades.ts
+++ src/app/components/icones-habilidades/icones-habilidades.ts
@@ -21,10 +21,10 @@ export class IconesHabilidades {
     { icone: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg', titulo: 'C++',},
     { icone: 'https://skillicons.dev/icons?i=cs&theme=dark', titulo: 'C#' },
     { icone: 'https://skillicons.dev/icons?i=nodejs&theme=dark', titulo: 'Node.js' },
-
     // Frameworks e ferramentas
     { icone: 'https://skillicons.dev/icons?i=bootstrap&theme=dark', titulo: 'Bootstrap' },
     { icone: 'https://skillicons.dev/icons?i=angular&theme=dark', titulo: 'Angular' },
+    { icone: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rxjs/rxjs-original.svg', titulo: 'RxJS' },
     { icone: 'https://skillicons.dev/icons?i=net&theme=dark', titulo: '.NET' },
     { icone: 'https://skillicons.dev/icons?i=git&theme=dark', titulo: 'Git' },
     { icone: 'https://skillicons.dev/icons?i=github&theme=dark', titulo: 'GitHub' },
@@ -35,7 +35,6 @@ export class IconesHabilidades {
     { icone: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/newrelic.svg', titulo: 'New Relic',},
     { icone: 'https://cdn.simpleicons.org/prettier?viewbox=auto', titulo: 'Prettier' },
     { icone: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eslint/eslint-original.svg', titulo: 'ESLint' },
-
     // Banco de Dados
     { icone: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg', titulo: 'MySQL', },
     { icone: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg', titulo: 'PostgreSQL', },
@@ -48,14 +47,12 @@ export class IconesHabilidades {
     { icone: 'https://skillicons.dev/icons?i=visualstudio&theme=dark', titulo: 'Visual Studio' },
     { icone: 'https://skillicons.dev/icons?i=vscode&theme=dark', titulo: 'VS Code' },
     { icone: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dbeaver/dbeaver-original.svg', titulo: 'DBeaver', },
-
     // Hosts e cloud
     { icone: 'https://skillicons.dev/icons?i=azure&theme=dark', titulo: 'Azure' },
     { icone: 'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/render.svg', titulo: 'Render', },
     { icone: 'https://railway.app/brand/logo-light.svg', titulo: 'Railway' },
-
     // Testes
     { icone: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/selenium/selenium-original.svg', titulo: 'Selenium', },
-    { icone: 'https://avatars.githubusercontent.com/u/915046?s=200&v=4', titulo: 'Mock' },
+    { icone: 'https://avatars.githubusercontent.com/u/915046?s=200&v=4', titulo: 'Moq' },
   ];
 }
