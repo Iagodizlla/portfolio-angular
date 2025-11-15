0225b84beb9578495dd502d9566c24ffc1e36e6b
iagodizlla <iagoschlemper@gmail.com>
2025-11-14 23:08:35 -0300

    Adicionado detalhes de desenvolvimento para projetos no portfólio

--- src/app/components/portfolio/itens-portifolio.ts
+++ src/app/components/portfolio/itens-portifolio.ts
@@ -15,6 +15,15 @@ titulo: 'Controle de Bar',
         'https://skillicons.dev/icons?i=javascript&theme=light',
       ],
       descricao: `O sistema de Controle de Bar é um projeto que visa o gerenciamento de um bar, com o controle de estoque, funcionários, clientes e pedidos. O sistema permite a criação de pedidos, a atualização do estoque e a visualização dos dados dos clientes e funcionários.`,
+      detalhesD: {
+        quantidadeCommits: 45,
+        statusProjeto: 'Concluído',
+        tempoDesenvolvimento: 'Jun 12, 2025 - Jun 25, 2025',
+        frequenciaContribuicoes: {
+          labels: ['12 Jun', '13 Jun', '14 Jun', '15 Jun', '16 Jun', '17 Jun', '18 Jun', '19 Jun', '20 Jun', '21 Jun', '22 Jun', '23 Jun', '24 Jun', '25 Jun'],
+          data: [8, 8, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2]
+        }
+      }
     },
     {
       titulo: 'Gestão de Equipamentos',
@@ -28,6 +37,15 @@ titulo: 'Controle de Bar',
         'https://skillicons.dev/icons?i=net&theme=light',
       ],
       descricao: `O sistema de Gestão de Equipamentos controla o estoque de um almoxarifado, com a gestão de equipamentos e o registro de chamados de manutenção que eventualmente ocorrem durante o uso dos equipamentos. Também são inclusas informações sobre os fabricantes dos equipamentos para contato de suporte ou compras.`,
+      detalhesD: {
+        quantidadeCommits: 0,
+        statusProjeto: 'Indeterminado',
+        tempoDesenvolvimento: '?? 2025 - ?? 2025',
+        frequenciaContribuicoes: {
+          labels: ['ano 1'],
+          data: [0]
+        }
+      }
     },
     {
       titulo: 'E-Agenda',
@@ -45,6 +63,15 @@ titulo: 'Controle de Bar',
         'https://skillicons.dev/icons?i=azure&theme=light',
       ],
       descricao: `A e-Agenda é um sistema web para gerenciar tarefas, contatos, compromissos, categorias e despesas. Permite cadastrar, editar, excluir e visualizar registros, com suporte a itens de tarefa, prioridades e controle de conclusão. Ideal para organização pessoal e profissional.`,
+      detalhesD: {
+        quantidadeCommits: 121,
+        statusProjeto: 'Concluído',
+        tempoDesenvolvimento: 'Jun 17, 2025 - Jul 18, 2025',
+        frequenciaContribuicoes: {
+          labels: ['17 Jun', '18 Jun', '19 Jun', '20 Jun', '21 Jun', '22 Jun', '23 Jun', '24 Jun', '25 Jun', '26 Jun', '27 Jun', '28 Jun', '29 Jun', '30 Jun', '1 Jul', '2 Jul', '3 Jul', '4 Jul', '5 Jul', '6 Jul', '7 Jul', '8 Jul', '9 Jul', '10 Jul', '11 Jul', '12 Jul', '13 Jul', '14 Jul', '15 Jul', '16 Jul', '17 Jul', '18 Jul'],
+          data: [9, 6, 6, 5, 4, 1, 2, 0, 0, 8, 3, 0, 1, 0, 0, 1, 1, 0, 1, 4, 0, 0, 0, 2, 2, 2, 0, 1, 0, 0, 0, 1]
+        }
+      }
     },
     {
       titulo: 'E-Agenda-Orm',
@@ -62,6 +89,15 @@ titulo: 'Controle de Bar',
         'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/render.svg',
       ],
       descricao: `A e-Agenda é um sistema web para gerenciar tarefas, contatos, compromissos, categorias e despesas. Permite cadastrar, editar, excluir e visualizar registros, com suporte a itens de tarefa, prioridades e controle de conclusão. Ideal para organização pessoal e profissional.`,
+      detalhesD: {
+        quantidadeCommits: 130,
+        statusProjeto: 'Concluído',
+        tempoDesenvolvimento: 'Jun 16, 2025 - Jul 18, 2025',
+        frequenciaContribuicoes: {
+          labels: ['16 Jun', '17 Jun', '18 Jun', '19 Jun', '20 Jun', '21 Jun', '22 Jun', '23 Jun', '24 Jun', '25 Jun', '26 Jun', '27 Jun', '28 Jun', '29 Jun', '30 Jun', '1 Jul', '2 Jul', ' 3 Jul', '4 Jul', '5 Jul', '6 Jul', '7 Jul', '8 Jul', '9 Jul', '10 Jul', '11 Jul', '12 Jul', '13 Jul', '14 Jul', '15 Jul', '16 Jul', '17 Jul', '18 Jul'],
+          data: [13, 9, 6, 6, 5, 4, 1, 2, 0, 0, 8, 3, 0, 1, 0, 0, 1, 1, 0, 1, 4, 0, 0, 0, 2, 2, 5, 5, 2, 0, 0, 0, 1]
+        }
+      }
     },
     {
       titulo: 'Controle de Cinema',
@@ -84,6 +120,15 @@ titulo: 'Controle de Bar',
         'https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/moq.svg',
       ],
       descricao: `O Controle de Cinema tem como objetivo gerenciar filmes, sessões e ingressos de forma prática e organizada, permitindo que empresas administrem filmes, salas e sessões, enquanto clientes podem visualizar opções, escolher lugares e comprar ingressos.`,
+      detalhesD: {
+        quantidadeCommits: 80,
+        statusProjeto: 'Concluído',
+        tempoDesenvolvimento: 'Aug 21, 2025 - Set 1, 2025',
+        frequenciaContribuicoes: {
+          labels: ['21 Aug', '22 Aug', '23 Aug', '24 Aug', '25 Aug', '26 Aug', '27 Aug', '28 Aug', '29 Aug', '30 Aug', '31 Aug', '1 Set'],
+          data: [3, 12, 0, 0, 1, 3, 8, 2, 2, 6, 3, 7]
+        }
+      }
     },
     {
       titulo: 'Site Pessoal',
@@ -96,6 +141,20 @@ titulo: 'Controle de Bar',
         'https://skillicons.dev/icons?i=javascript&theme=light',
       ],
       descricao: `Este é o meu site pessoal, onde compartilho informações sobre mim, meu server de Minecraft e minigames, como torneios e batalhas em equipes.`,
+      detalhesD: {
+        quantidadeCommits: 60,
+        statusProjeto: 'Em desenvolvimento',
+        tempoDesenvolvimento: 'Mar 17 2025 - ...',
+        frequenciaContribuicoes: {
+          labels: ['17 Mar', '18 Mar', '19 Mar', '20 Mar', '21 Mar', '22 Mar', '23 Mar', '24 Mar', '25 Mar', '26 Mar', '27 Mar', '28 Mar', '29 Mar', '30 Mar', '31 Mar',
+             '1 Abr', '2 Abr', '3 Abr', '4 Abr', '5 Abr', '6 Abr', '7 Abr', '8 Abr', '9 Abr', '10 Abr', '11 Abr', '12 Abr', '13 Abr', '14 Abr', '15 Abr', '16 Abr', '17 Abr', '18 Abr', '19 Abr', '20 Abr', '21 Abr', '22 Abr', '23 Abr', '24 Abr', '25 Abr', '26 Abr', '27 Abr', '28 Abr', '29 Abr', '30 Abr', '31 Abr',
+             '1 Mai', '2 Mai', '3 Mai', '4 Mai', '5 Mai', '6 Mai', '7 Mai', '8 Mai', '9 Mai', '10 Mai', '11 Mai', '12 Mai', '13 Mai', '14 Mai', '15 Mai', '16 Mai', '17 Mai', '18 Mai', '19 Mai', '20 Mai', '21 Mai', '22 Mai', '23 Mai', '24 Mai', '25 Mai', '26 Mai', '27 Mai', '28 Mai', '29 Mai', '30 Mai', '31 Mai',
+             '1 Jun', '2 Jun', '3 Jun', '4 Jun', ' Jun5', '6 Jun', '7 Jun', '8 Jun', '9 Jun', '10 Jun', '11 Jun', '12 Jun', '13 Jun', '14 Jun', '15 Jun', '16 Jun', '17 Jun', '18 Jun', '19 Jun', '20 Jun', '21 Jun', '22 Jun', '23 Jun', '24 Jun', '25 Jun', '26 Jun', '27 Jun', '28 Jun', '29 Jun', '30 Jun',
+             '1 Jul', '2 Jul', '3 Jul', '4 Jul', '5 Jul', '6 Jul', '7 Jul', '8 Jul', '9 Jul', '10 Jul', '11 Jul', '12 Jul', '13 Jul', '14 Jul', '15 Jul', '16 Jul', '17 Jul', '18 Jul', '19 Jul', '20 Jul', '21 Jul', '22 Jul', '23 Jul', '24 Jul', '25 Jul', '26 Jul', '27 Jul', '28 Jul', '29 Jul', '30 Jul', '31 Jul',
+             '1 Aug', '2 Aug', '3 Aug', '4 Aug', '5 Aug', '6 Aug', '7 Aug', '8 Aug', '9 Aug', '10 Aug', '11 Aug', '12 Aug', '13 Aug', '14 Aug', '15 Aug', '16 Aug', '17 Aug', '18 Aug'],
+          data: [7, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  1, 11, 8, 2, 8, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 1, 7, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3]
+        }
+      }
     },
     {
       titulo: 'Pokédex',
@@ -114,6 +173,15 @@ titulo: 'Controle de Bar',
         'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rxjs/rxjs-original.svg',
       ],
       descricao: `Pokédex desenvolvida em Angular, com funcionalidades de favoritar Pokémon, exibição de detalhes como sprites, sons e tipos. Projeto com foco em consumo de API, interatividade e design responsivo.`,
+      detalhesD: {
+        quantidadeCommits: 24,
+        statusProjeto: 'Concluído',
+        tempoDesenvolvimento: 'Sep 29, 2025 - Oct 9, 2025',
+        frequenciaContribuicoes: {
+          labels: ['29 Sep', '30 Sep', '1 Oct', '2 Oct', '3 Oct', '4 Oct', '5 Oct', '6 Oct', '7 Oct', '8 Oct', '9 Oct'],
+          data: [1, 0, 0, 0, 9, 1, 0, 7, 1, 1, 4]
+        }
+      }
     },
     {
       titulo: 'Jogo de Adivinhação',
@@ -129,7 +197,15 @@ titulo: 'Controle de Bar',
         'https://skillicons.dev/icons?i=bootstrap&theme=light',
       ],
       descricao: `Jogo interativo de adivinhação de números com níveis de dificuldade ajustáveis. Permite que o usuário escolha a dificuldade e mantém um ranking local por nível, oferecendo feedback em tempo real.`,
-    },
+      detalhesD: {
+        quantidadeCommits: 5,
+        statusProjeto: 'Concluído',
+        tempoDesenvolvimento: 'Sep 29, 2025 - Sep 30, 2025',
+        frequenciaContribuicoes: {
+          labels: ['29 Sep', '30 Sep'],
+          data: [1, 4]
+        }
+      }},
     {
       titulo: 'Organiza Med',
       urlImagem: 'projetos/organiza-med.png',
@@ -148,7 +224,15 @@ titulo: 'Controle de Bar',
         'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain.svg',
       ],
       descricao: `O Organiza Med é um sistema web para gerenciamento de consultas médicas, permitindo agendamento, cancelamento e visualização de consultas. Ideal para clínicas e consultórios otimizarem o atendimento aos pacientes.`,
-    },
+      detalhesD: {
+        quantidadeCommits: 30,
+        statusProjeto: 'Em desenvolvimento',
+        tempoDesenvolvimento: 'Nov 12, 2025 - ...',
+        frequenciaContribuicoes: {
+          labels: ['12 Nov', '13 Nov', '14 Nov'],
+          data: [11, 11, 3]
+        }
+      }},
     {
       titulo: 'Note Keeper',
       urlImagem: 'projetos/note-keeper.png',
@@ -169,7 +253,17 @@ titulo: 'Controle de Bar',
          'https://skillicons.dev/icons?i=azure&theme=light',
       ],
       descricao: `O Note Keeper é um sistema web para criação, edição e organização de notas. Permite gerenciar anotações de forma simples e rápida, com CRUD completo integrado entre Angular e C#. Ideal para registrar e consultar informações do dia a dia.`,
-    },{
+      detalhesD: {
+        quantidadeCommits: 55,
+        statusProjeto: 'Concluído',
+        tempoDesenvolvimento: 'Oct 30, 2025 - Nov 7, 2025',
+        frequenciaContribuicoes: {
+          labels: ['30 Oct', '31 Oct', '1 Nov', '2 Nov', '3 Nov', '4 Nov', '5 Nov', '6 Nov', '7 Nov'],
+          data: [4, 34, 0, 0, 1, 5, 0, 7, 4]
+        }
+      }
+    },
+    {
       titulo: 'Academia do Zé',
       urlImagem: 'projetos/em-desenvolvimento.png',
       urlGif: 'https://i.imgur.com/SEU_GIF.gif',
@@ -181,5 +275,18 @@ titulo: 'Controle de Bar',
         'https://skillicons.dev/icons?i=docker&theme=light',
       ],
       descricao: `O sistema Academia do Zé é uma aplicação para gerenciar as operações de uma academia, incluindo cadastro de alunos, controle de mensalidades, agendamento de aulas e gerenciamento de instrutores. Projetado para otimizar a administração e melhorar a experiência dos clientes.`,
+      detalhesD: {
+        quantidadeCommits: 55,
+        statusProjeto: 'Em desenvolvimento',
+        tempoDesenvolvimento: 'Jul 17, 2025 - Fev 2024',
+        frequenciaContribuicoes: {
+          labels: ['17 Jul', '18 Jul', '19 Jul', '20 Jul', '21 Jul', '22 Jul', '23 Jul', '24 Jul', '25 Jul', '26 Jul', '27 Jul', '28 Jul', '29 Jul', '30 Jul', '31 Jul',
+             '1 Aug', '2 Aug', '3 Aug', '4 Aug', '5 Aug', '6 Aug', '7 Aug', '8 Aug', '9 Aug', '10 Aug', '11 Aug', '12 Aug', '13 Aug', '14 Aug', '15 Aug', '16 Aug', '17 Aug', '18 Aug', '19 Aug', '20 Aug', '21 Aug', '22 Aug', '23 Aug', '24 Aug', '25 Aug', '26 Aug', '27 Aug', '28 Aug', '29 Aug', '30 Aug', '31 Aug',
+             '1 Sep', '2 Sep', '3 Sep', '4 Sep', '5 Sep', '6 Sep', '7 Sep', '8 Sep', '9 Sep', '10 Sep', '11 Sep', '12 Sep', '13 Sep', '14 Sep', '15 Sep', '16 Sep', '17 Sep', '18 Sep', '19 Sep', '20 Sep', '21 Sep', '22 Sep', '23 Sep', '24 Sep', '25 Sep', '26 Sep', '27 Sep', '28 Sep', '29 Sep', '30 Sep',
+             '1 Oct', '2 Oct', '3 Oct', '4 Oct', '5 Oct', '6 Oct', '7 Oct', '8 Oct', '9 Oct', '10 Oct'
+          ],
+          data: [2, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 3, 4, 0, 0, 0, 1, 0, 0, 3, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 0, 0, 2, 5, 5, 0, 0, 0, 0, 0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
+        }
+      }
     },
 ];
