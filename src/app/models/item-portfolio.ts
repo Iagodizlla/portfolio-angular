3b7cb619ccfee6e69f6ceca4663dfdda7b59f4f5
iagodizlla <iagoschlemper@gmail.com>
2025-11-14 22:38:56 -0300

    Instalado biblioteca chart.js

--- src/app/models/item-portfolio.ts
+++ src/app/models/item-portfolio.ts
@@ -5,4 +5,15 @@ export interface ItemPortfolio {
   urlGif: string;
   urlRepositorio: string;
   stack: string[];
+  detalhesD?: DetalhesD;
+}
+
+export interface DetalhesD{
+  quantidadeCommits?: number;         // Total de commits
+  frequenciaContribuicoes?: {         // Para gráfico de commits por período
+    labels: string[];                 // Ex: ['Semana 1', 'Semana 2', ...] ou ['Jan', 'Feb']
+    data: number[];                   // Quantidade de commits correspondente
+  };
+  statusProjeto?: string;             // Ex: 'Em desenvolvimento', 'Concluído'
+  tempoDesenvolvimento?: string;      // Ex: '2 meses', 'Jan 2024 - Mar 2024'
 }
